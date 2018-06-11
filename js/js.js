1ea12aa444c72bbf8e9b8774b6582e232eecb1c2
quentin mccarthy <845868401@bun2-206-14.local>
2018-06-12 10:00:06 +1200

    Working on custom markers for hotels

--- js/js.js
+++ js/js.js
@@ -148,7 +148,94 @@ $(document).ready(function(){
 	// Mapbox
 	mapboxgl.accessToken = 'pk.eyJ1IjoibWNjYXJ0aHlxIiwiYSI6ImNqaTNucHpsMzAwaGczcXF2eDJhbGxwNGwifQ.Qn-qvcjlEmkiLq4lqV435A';
 	var map = new mapboxgl.Map({
-		container: 'map',
-		style: 'mapbox://styles/mccarthyq/cji9h1jw0124i2sqitzoczzsh'
+		container:"map", // container id
+		style:"mapbox://styles/mccarthyq/cji9h1jw0124i2sqitzoczzsh",
+		center:[171.285471,-41.090805], // starting position
+		zoom:4 // starting zoom
 	});
+
+	// Add geolocate control to the map.
+	map.addControl(new mapboxgl.GeolocateControl({
+		positionOptions:{
+			enableHighAccuracy:true
+		},
+		trackUserLocation:true
+	}));
+
+	// geojson data for locations
+	$.getJSON("assets/sweetgreen.geojson", function(data){
+		var locations = data;
+
+		map.on("load",function(e){
+			// Add the data to your map as a layer
+			map.addLayer({
+				id:"locations",
+				type:"symbol",
+				// Add a GeoJSON source containing place coordinates and information.
+				source:{
+					type:"geojson",
+					data:locations
+				},
+				layout:{
+					"icon-image":"restaurant-15",
+					"icon-allow-overlap":true
+				}
+			});
+
+			buildLocationList(locations);
+		});
+	});
+
+	function buildLocationList(data){
+		// Iterate through the list of stores
+		for(var i = 0; i < data.features.length; i++){
+			var currentFeature = data.features[i];
+			// Shorten data.feature.properties to just "prop" so we're not writing
+			// this long form over and over again.
+			var prop = currentFeature.properties;
+			// Select the listing container in the HTML and append a div
+			// with the class "item" for each store
+			var listings = $("#listings");
+			var listing = $("<div>", {"className":"item","id":"listing-"+i})
+				.append($("<a>", {"href":"#","className":"title","dataPosition":i,})
+					.html(prop.address)
+					.on("click",function(e){
+						// Update the currentFeature to the store associated with the clicked link
+						var clickedListing = data.features[$(this).attr("dataPosition")];
+						// 1. Fly to the point associated with the clicked link
+						flyToStore(clickedListing);
+						// 2. Close all other popups and display popup for clicked store
+						createPopUp(clickedListing);
+						// 3. Highlight listing in sidebar (and remove highlight for all other listings)
+						var activeItem = $(".active");
+						if(activeItem[0]){
+							$(activeItem[0]).removeClass("active");
+						}
+						$(this.parentNode).addClass("active");
+					})
+				)
+				.append($("<div>").html(prop.city+" &middot; "+prop.phoneFormatted))
+				.appendTo(listings);
+		}
+	}
+
+	function flyToStore(currentFeature){
+		map.flyTo({
+			center:currentFeature.geometry.coordinates,
+			zoom:15
+		});
+	}
+
+	function createPopUp(currentFeature){
+		var popUps = $(".mapboxgl-popup");
+		// Check if there is already a popup on the map and if so, remove it
+		if(popUps[0]){
+			popUps[0].remove();
+		}
+
+		var popup = new mapboxgl.Popup({closeOnClick:false})
+			.setLngLat(currentFeature.geometry.coordinates)
+			.setHTML("<h3>Sweetgreen</h3>"+"<h4>"+currentFeature.properties.address+"</h4>")
+			.addTo(map);
+	}
 });
