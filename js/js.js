ae36ebdf107f901bbe88ad6dde04a7400b420c1d
quentin mccarthy <845868401@bun2-206-14.local>
2018-06-20 11:28:09 +1200

    Moved mapbox functions into mapbox object for convenience.

--- js/js.js
+++ js/js.js
@@ -1,7 +1,162 @@
 var app = {
+	locationData:{},
 	dataConfig:{},
 	formResults:{},
-	map:null,
+	mapbox:{
+		map:null,
+		initMapbox:function(){
+			// Mapbox functions for initial setup of mapbox
+			mapboxgl.accessToken = 'pk.eyJ1IjoibWNjYXJ0aHlxIiwiYSI6ImNqaTNucHpsMzAwaGczcXF2eDJhbGxwNGwifQ.Qn-qvcjlEmkiLq4lqV435A';
+			var map = new mapboxgl.Map({
+				container:"map", // container id
+				style:"mapbox://styles/mccarthyq/cji9h1jw0124i2sqitzoczzsh",
+				center:[174.777623,-41.289632], // starting position
+				zoom:12 // starting zoom
+			});
+
+			// Keep track of the map as a variable in the app for use later on
+			app.mapbox.map = map;
+
+			// Add geolocate control to the map.
+			map.addControl(new mapboxgl.GeolocateControl({
+				positionOptions:{
+					enableHighAccuracy:true
+				},
+				trackUserLocation:true
+			}));
+		},
+		createMarkers:function(hotels,hostels,motels,houses){
+			// Object to imitate geojson data structure
+			var locations = {
+				"type":"FeatureCollection",
+			    "features":[]
+			};
+
+			var features = app.locationData.features;
+
+			for(var i = 0; i < features.length; i++){
+				var currType = features[i].properties.type;
+
+				if((hotels && currType == "hotel") || (hostels && currType == "hostel") || (motels && currType == "motel") || (houses && currType == "house")){
+					locations.features.push(features[i])
+				}
+			}
+
+			// Add the data to your map as a layer
+			app.mapbox.map.addSource("places", {
+				type:"geojson",
+				data:locations
+			});
+
+			locations.features.forEach(function(marker){
+				// Create a div element for the marker
+				var el = document.createElement("div");
+				// Add a class called "marker" to each div
+				el.className = "marker";
+				// By default the image for your custom marker will be anchored
+				// by its center. Adjust the positoon accordingly
+				// Create the custom markers, set their position and add to map
+				new mapboxgl.Marker(el, {offset:[0,-23]})
+					.setLngLat(marker.geometry.coordinates)
+					.addTo(app.mapbox.map);
+
+				$(el).on("click",function(e){
+					var activeItem = $(".active");
+					// 1. Fly to the point
+					app.mapbox.flyToStore(marker);
+					// 2. Close all other popups and display popup for clicked store
+					app.mapbox.createPopUp(marker);
+					// 3. Highlight listin in sidebar (and remove highlight for all other listings)
+					e.stopPropagation();
+					if(activeItem[0]){
+						$(activeItem[0]).removeClass("active");
+					}
+
+					// var listing = $("#listing-"+i);
+					// $(listing).addClass("active");
+				});
+			});
+
+			app.mapbox.buildLocationList(locations);
+		},
+		buildLocationList:function(data){
+			// Iterate through the list of stores
+			for(var i = 0; i < data.features.length; i++){
+				var currentFeature = data.features[i];
+				// Shorten data.feature.properties to just "prop" so we're not writing
+				// this long form over and over again.
+				var prop = currentFeature.properties;
+				// Select the listing container in the HTML and append a div
+				// with the class "item" for each store
+				var listings = $("#listings");
+				var listing = $("<a>", {"class":"dropdown-item","href":"#","id":"listing-"+i,"dataPosition":i})
+					.html(prop.name)
+					.on("click",function(){
+						// Update the currentFeature to the store associated with the clicked link
+						var clickedListing = data.features[$(this).attr("dataPosition")];
+						// 1. Fly to the point associated with the clicked link
+						app.mapbox.flyToStore(clickedListing);
+						// 2. Close all other popups and display popup for clicked store
+						app.mapbox.createPopUp(clickedListing);
+						// 3. Highlight listing in sidebar (and remove highlight for all other listings)
+						// var activeItem = $(".active");
+						// if(activeItem[0]){
+							// $(activeItem[0]).removeClass("active");
+						// }
+						// $(this).addClass("active");
+
+						var prop = data.features[$(this).attr("dataPosition")].properties;
+
+						var locationInfo = $("#locationInfo");
+
+						$(locationInfo).html("");
+
+						$("<h3>").text(prop.name).appendTo(locationInfo);
+
+						switch(prop.type){
+							case "hotel":
+								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[0].properties.costNightly).appendTo(locationInfo);
+
+								break;
+							case "hostel":
+								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[1].properties.costNightly).appendTo(locationInfo);
+
+								break
+							case "motel":
+								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[2].properties.costNightly).appendTo(locationInfo);
+
+								break
+							case "house":
+								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[3].properties.costNightly).appendTo(locationInfo);
+
+								break
+							default:
+
+						};
+					})
+					// .append($("<div>").html(prop.city+" &middot; "+prop.phoneFormatted))
+					.appendTo(listings);
+			}
+		},
+		flyToStore:function(currentFeature){
+			app.mapbox.map.flyTo({
+				center:currentFeature.geometry.coordinates,
+				zoom:15
+			});
+		},
+		createPopUp:function(currentFeature){
+			var popUps = $(".mapboxgl-popup");
+			// Check if there is already a popup on the map and if so, remove it
+			if(popUps[0]){
+				popUps[0].remove();
+			}
+
+			var popup = new mapboxgl.Popup({closeOnClick:true})
+				.setLngLat(currentFeature.geometry.coordinates)
+				.setHTML("<h5>"+currentFeature.properties.name+"</h5>"+"<h6>"+currentFeature.properties.address+"</h6>")
+				.addTo(app.mapbox.map);
+		}
+	},
 	init:function(){
 		$.getJSON("assets/dataConfig.json", function(data){
 			app.dataConfig = data;
@@ -114,6 +269,64 @@ var app = {
 				var desiredSpace = app.formResults.desiredSpace;
 				var desiredDays = app.formResults.desiredDays;
 
+				var hotels = false;
+				var hostels = false;
+				var motels = false;
+				var houses = false;
+
+				// The expressions in these variables return a boolean value
+				// This process has been broken down just to make it easy to read and work with
+				// 0. Hotel
+				var hotelMinSpace = (desiredSpace >= app.dataConfig.types[0].properties.minSpace);
+				var hotelMaxSpace = (desiredSpace <= app.dataConfig.types[0].properties.maxSpace);
+				var hotelMinStay = (desiredDays >= app.dataConfig.types[0].properties.minStay);
+				var hotelMaxStay = (desiredDays <= app.dataConfig.types[0].properties.maxStay);
+
+				var hotelSpace = (hotelMinSpace && hotelMaxSpace);
+				var hotelStay = (hotelMinStay && hotelMaxStay);
+
+				// 1. Hostel
+				var hostelMinSpace = (desiredSpace >= app.dataConfig.types[1].properties.minSpace);
+				var hostelMaxSpace = (desiredSpace <= app.dataConfig.types[1].properties.maxSpace);
+				var hostelMinStay = (desiredDays >= app.dataConfig.types[1].properties.minStay);
+				var hostelMaxStay = (desiredDays <= app.dataConfig.types[1].properties.maxStay);
+
+				var hostelSpace = (hostelMinSpace && hostelMaxSpace);
+				var hostelStay = (hostelMinStay && hostelMaxStay);
+
+				// 2. Motel
+				var motelMinSpace = (desiredSpace >= app.dataConfig.types[2].properties.minSpace);
+				var motelMaxSpace = (desiredSpace <= app.dataConfig.types[2].properties.maxSpace);
+				var motelMinStay = (desiredDays >= app.dataConfig.types[2].properties.minStay);
+				var motelMaxStay = (desiredDays <= app.dataConfig.types[2].properties.maxStay);
+
+				var motelSpace = (motelMinSpace && motelMaxSpace);
+				var motelStay = (motelMinStay && motelMaxStay);
+
+				// 3. House
+				var houseMinSpace = (desiredSpace >= app.dataConfig.types[3].properties.minSpace);
+				var houseMaxSpace = (desiredSpace <= app.dataConfig.types[3].properties.maxSpace);
+				var houseMinStay = (desiredDays >= app.dataConfig.types[3].properties.minStay);
+				var houseMaxStay = (desiredDays <= app.dataConfig.types[3].properties.maxStay);
+
+				var houseSpace = (houseMinSpace && houseMaxSpace);
+				var houseStay = (houseMinStay && houseMaxStay);
+
+				if(hotelSpace && hotelStay){
+					hotels = true;
+				}
+				if(hostelSpace && hostelStay){
+					hostels = true;
+				}
+				if(motelSpace && motelStay){
+					motels = true;
+				}
+				if(houseSpace && houseStay){
+					houses = true;
+				}
+
+				app.mapbox.createMarkers(hotels,hostels,motels,houses)
+
 				// Validation; the step shouldn't change if either of these values is undefined
 				if((desiredSpace) && (desiredDays)){
 					// Switch the steps. Bootstrap display class used.
@@ -142,170 +355,31 @@ var app = {
 			});
 		});
 
-		$(".currStep").css("width",$(".main-navbar .col-sm div").css("width"));
-		$(".currStep").css("height",$(".main-navbar .col-sm div").css("height"));
-
-		app.initMapbox();
-
-		// All steps are visible initially to allow proper loading of elements such as mapbox
-		// If the steps were hidden initially, certain elements would not style themselves properly.
-		$(".step-2").addClass("d-none");
-		$(".step-3").addClass("d-none");
-
-		// This code is just for development purposes and should be removed later on
-		console.log("Development");
-		$(".step-1").addClass("d-none");
-		$(".step-2").removeClass("d-none");
-		$(".currStep").css("left",((parseInt($(".currStep").css("left")))+(parseInt($(".main-navbar .col-sm div").css("width"))))+"px");
-		app.formResults.desiredSpace = 5;
-		app.formResults.desiredDays = 5;
-	},
-	initMapbox:function(){
-		// Mapbox functions for initial setup of mapbox
-		mapboxgl.accessToken = 'pk.eyJ1IjoibWNjYXJ0aHlxIiwiYSI6ImNqaTNucHpsMzAwaGczcXF2eDJhbGxwNGwifQ.Qn-qvcjlEmkiLq4lqV435A';
-		var map = new mapboxgl.Map({
-			container:"map", // container id
-			style:"mapbox://styles/mccarthyq/cji9h1jw0124i2sqitzoczzsh",
-			center:[174.777623,-41.289632], // starting position
-			zoom:12 // starting zoom
-		});
-
-		// Keep track of the map as a variable in the app for use later on
-		app.map = map;
-
-		// Add geolocate control to the map.
-		map.addControl(new mapboxgl.GeolocateControl({
-			positionOptions:{
-				enableHighAccuracy:true
-			},
-			trackUserLocation:true
-		}));
-
-		// geojson data for locations
 		$.getJSON("assets/accomodation.geojson", function(data){
 			var locations = data;
 
 			locations.features = app.sortData(locations.features);
 
-			map.on("load",function(e){
-				// Add the data to your map as a layer
-				map.addSource("places", {
-					type:"geojson",
-					data:locations
-				});
+			app.locationData = locations;
 
-				locations.features.forEach(function(marker){
-					// Create a div element for the marker
-					var el = document.createElement("div");
-					// Add a class called "marker" to each div
-					el.className = "marker";
-					// By default the image for your custom marker will be anchored
-					// by its center. Adjust the positoon accordingly
-					// Create the custom markers, set their position and add to map
-					new mapboxgl.Marker(el, {offset:[0,-23]})
-						.setLngLat(marker.geometry.coordinates)
-						.addTo(map);
+			app.mapbox.initMapbox();
 
-					$(el).on("click",function(e){
-						var activeItem = $(".active");
-						// 1. Fly to the point
-						flyToStore(marker);
-						// 2. Close all other popups and display popup for clicked store
-						createPopUp(marker);
-						// 3. Highlight listin in sidebar (and remove highlight for all other listings)
-						e.stopPropagation();
-						if(activeItem[0]){
-							$(activeItem[0]).removeClass("active");
-						}
+			// All steps are visible initially to allow proper loading of elements such as mapbox
+			// If the steps were hidden initially, certain elements would not style themselves properly.
+			$(".step-2").addClass("d-none");
+			$(".step-3").addClass("d-none");
 
-						// var listing = $("#listing-"+i);
-						// $(listing).addClass("active");
-					});
-				});
-
-				buildLocationList(locations);
-			});
+			// This code is just for development purposes and should be removed later on
+			console.log("Dev Mode");
+			$(".step-1").addClass("d-none");
+			$(".step-2").removeClass("d-none");
+			$(".currStep").css("left",((parseInt($(".currStep").css("left")))+(parseInt($(".main-navbar .col-sm div").css("width"))))+"px");
+			app.formResults.desiredSpace = 5;
+			app.formResults.desiredDays = 5;
 		});
 
-		function buildLocationList(data){
-			// Iterate through the list of stores
-			for(var i = 0; i < data.features.length; i++){
-				var currentFeature = data.features[i];
-				// Shorten data.feature.properties to just "prop" so we're not writing
-				// this long form over and over again.
-				var prop = currentFeature.properties;
-				// Select the listing container in the HTML and append a div
-				// with the class "item" for each store
-				var listings = $("#listings");
-				var listing = $("<a>", {"class":"dropdown-item","href":"#","id":"listing-"+i,"dataPosition":i})
-					.html(prop.name)
-					.on("click",function(){
-						// Update the currentFeature to the store associated with the clicked link
-						var clickedListing = data.features[$(this).attr("dataPosition")];
-						// 1. Fly to the point associated with the clicked link
-						flyToStore(clickedListing);
-						// 2. Close all other popups and display popup for clicked store
-						createPopUp(clickedListing);
-						// 3. Highlight listing in sidebar (and remove highlight for all other listings)
-						// var activeItem = $(".active");
-						// if(activeItem[0]){
-							// $(activeItem[0]).removeClass("active");
-						// }
-						// $(this).addClass("active");
-
-						var prop = data.features[$(this).attr("dataPosition")].properties;
-
-						var locationInfo = $("#locationInfo");
-
-						$(locationInfo).html("");
-
-						$("<h3>").text(prop.name).appendTo(locationInfo);
-
-						switch(prop.type){
-							case "hotel":
-								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[0].properties.costNightly).appendTo(locationInfo);
-
-								break;
-							case "hostel":
-								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[1].properties.costNightly).appendTo(locationInfo);
-
-								break
-							case "motel":
-								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[2].properties.costNightly).appendTo(locationInfo);
-
-								break
-							case "house":
-								$("<div>").text("Nightly Cost: $"+app.dataConfig.types[3].properties.costNightly).appendTo(locationInfo);
-
-								break
-							default:
-
-						};
-					})
-					// .append($("<div>").html(prop.city+" &middot; "+prop.phoneFormatted))
-					.appendTo(listings);
-			}
-		}
-
-		function flyToStore(currentFeature){
-			map.flyTo({
-				center:currentFeature.geometry.coordinates,
-				zoom:15
-			});
-		}
-
-		function createPopUp(currentFeature){
-			var popUps = $(".mapboxgl-popup");
-			// Check if there is already a popup on the map and if so, remove it
-			if(popUps[0]){
-				popUps[0].remove();
-			}
-
-			var popup = new mapboxgl.Popup({closeOnClick:true})
-				.setLngLat(currentFeature.geometry.coordinates)
-				.setHTML("<h5>"+currentFeature.properties.name+"</h5>"+"<h6>"+currentFeature.properties.address+"</h6>")
-				.addTo(map);
-		}
+		$(".currStep").css("width",$(".main-navbar .col-sm div").css("width"));
+		$(".currStep").css("height",$(".main-navbar .col-sm div").css("height"));
 	},
 	sortData:function(data){
 		var nameArray = [];
